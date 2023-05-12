02d86d8899bf810ec38e54d6c30388477b099959
pedrooryan <reametazgamer@gmail.com>
2023-05-12 20:33:40 -0300

    tabela e cards estilizados

--- pages/deputados/[id].js
+++ pages/deputados/[id].js
@@ -1,52 +1,59 @@
 import Pagina from '@/components/Pagina'
 import apiDeputados from '@/services/apiDeputados'
 import React from 'react'
-import { Alert, Card, Table } from 'react-bootstrap'
+import { Alert, Card, Col, Row, Table } from 'react-bootstrap'
 
 import styles from '../../styles/[id].module.css'
 
 const Detalhes = ({ deputadoDt, despesas }) => {
   return (
     <>
-    <Pagina titulo='Detalhes do Deputado'>
-      <Card className={styles.detalhescard} border="success">
-        <Card.Header className='bg-success text-white text-center p-2'><h3><strong>{deputadoDt.ultimoStatus.nome}</strong></h3></Card.Header>
+      <Pagina titulo='Detalhes do Deputado'>
+        <Row>
+        <Col md={4}>
+        <Card className={styles.detalhescard} border="success">
+          <Card.Header className='bg-success text-white text-center p-2'><h3><strong>{deputadoDt.ultimoStatus.nome}</strong></h3></Card.Header>
           <Card.Img variant="top" src={deputadoDt.ultimoStatus.urlFoto} />
-            <Card.Body>
+          <Card.Body>
             <Card.Title className={styles.textcard} ><strong>Informações</strong></Card.Title>
             <br></br>
-              <Card.Text className={styles.textcard}><strong>Email: </strong>{deputadoDt.ultimoStatus.gabinete.email}</Card.Text>
-              <Card.Text className={styles.textcard}><strong>Data de nascimento: </strong>{new Date (deputadoDt.dataNascimento).toLocaleDateString('pt-BR')}</Card.Text>
-              <Card.Text className={styles.textcard}><strong>Telefone: </strong>{deputadoDt.ultimoStatus.gabinete.telefone}</Card.Text>
-              <Card.Text className={styles.textcard}><strong>Situação: </strong>{deputadoDt.ultimoStatus.situacao}</Card.Text>
-            </Card.Body>
-      </Card> 
-      <br></br>
-
-      <Alert className={styles.textalert}>
+            <Card.Text className={styles.textcard}><strong>Email: </strong>{deputadoDt.ultimoStatus.gabinete.email}</Card.Text>
+            <Card.Text className={styles.textcard}><strong>Data de nascimento: </strong>{new Date(deputadoDt.dataNascimento).toLocaleDateString('pt-BR')}</Card.Text>
+            <Card.Text className={styles.textcard}><strong>Telefone: </strong>{deputadoDt.ultimoStatus.gabinete.telefone}</Card.Text>
+            <Card.Text className={styles.textcard}><strong>Situação: </strong>{deputadoDt.ultimoStatus.situacao}</Card.Text>
+          </Card.Body>
+        </Card>
+        </Col>
+        <br></br>
+        <Col md={8}>
+        <Alert className={styles.textalert}>
           <h2><strong>Gastos</strong></h2>
         </Alert>
 
-      <br></br>
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th className='text-center'>Data</th>
-              <th className='text-center'>Descrição</th>
+        <br></br>
+        <Card className={styles.tabledetalhes}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th className='text-center'>Data</th>
+                <th className='text-center'>Descrição</th>
                 <th className='text-center'>Valor</th>
-                  </tr>
-                    </thead>
-                      <tbody>
-                        {despesas.map(item => (
-                          <tr key={item.id}>
-                            <td className='text-center'>{new Date(item.dataDocumento).toLocaleDateString('pt-BR')}</td>
-                              <td className='text-center'>{item.tipoDespesa}</td>
-                              <td className='text-center'>{item.valorDocumento.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</td>
-                            </tr>
-                          ))}
-                     </tbody>
-            </Table>
-    </Pagina>
+              </tr>
+            </thead>
+            <tbody>
+              {despesas.map(item => (
+                <tr key={item.id}>
+                  <td className='text-center'>{new Date(item.dataDocumento).toLocaleDateString('pt-BR')}</td>
+                  <td className='text-center'>{item.tipoDespesa}</td>
+                  <td className='text-center'>{item.valorDocumento.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card>
+        </Col>
+        </Row>  
+      </Pagina>
     </>
   )
 }
@@ -54,7 +61,7 @@ const Detalhes = ({ deputadoDt, despesas }) => {
 export default Detalhes
 
 export async function getServerSideProps(context) {
-  
+
   const id = context.params.id
 
   const resultado = await apiDeputados.get('/deputados/' + id)
@@ -65,6 +72,6 @@ export async function getServerSideProps(context) {
 
 
   return {
-      props: { deputadoDt, despesas },
+    props: { deputadoDt, despesas },
   }
 }
